44861cec0ba969fac7964649cd88d7b34c392426
tampm92 <phanminhtam1992@gmail.com>
2022-09-25 14:30:21 +0000

    Deploying to gh-pages from @ petiteasie/website@5954fd7eccb492ebc5380c75051738d3058ef62c 🚀

---
+++ _next/static/13qtHfgjA52zuGqABFqY1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/664-97b4fd2a20e416ac.js","static/chunks/pages/index-29a941d9bae05258.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- _next/static/dc3pzs4PsawNPTlUvErH7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/664-97b4fd2a20e416ac.js","static/chunks/pages/index-205aa103bf7be216.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
