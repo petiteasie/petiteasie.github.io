c4d9f3f4a9072f05fe62d292756e674626226015
tampm92 <phanminhtam1992@gmail.com>
2023-12-13 05:05:42 +0000

    Deploying to gh-pages from @ petiteasie/website@d048e644391aacd342813c22c2abd0019abaa519 🚀

--- _next/static/M9OO2_fOEpDdqVDrsdJ1-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/664-97b4fd2a20e416ac.js","static/chunks/pages/index-820d711786681d70.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
